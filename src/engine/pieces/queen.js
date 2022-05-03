e6bc4fc7ded0f4db90d659af1346315085a5ff59
chidin194 <chiara.dinan@softwire.com>
2022-05-03 20:54:10 +0100

    Chessington: Limit Queen's moves so piece cannot go past other pieces

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -25,6 +25,18 @@ export default class Queen extends Bishop {
             if (i !== location.row) { moves.push(move); }
         }
 
-        return moves.concat(availableMoves);
+        let  piecePosition = '';
+
+        for(const move of moves) {
+            if (board.getPiece(move)) {
+                piecePosition = move;
+            }
+        }
+
+        if(piecePosition) {
+            return moves.splice(0, 0, moves.indexOf(piecePosition)).concat(availableMoves);
+        } else {
+            return moves.concat(availableMoves);
+        }
     }
 }
