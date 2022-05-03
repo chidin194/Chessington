08032bb1762ed47ea94842cd20eba10ba5caf364
chidin194 <chiara.dinan@softwire.com>
2022-05-03 13:17:01 +0100

    Chessington: Add knight functionality

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from "../square";
 
 export default class Knight extends Piece {
     constructor(player) {
@@ -6,6 +7,28 @@ export default class Knight extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+
+        let location = board.findPiece(this)
+        let r = location.row;
+        let c = location.col;
+
+        const possibleMoves = [
+            Square.at(r+1, c+2),Square.at(r+2, c+1), Square.at(r+1, c-2), Square.at(r+2, c-1),
+            Square.at(r-1, c+2), Square.at(r-2, c+1), Square.at(r-1, c-2), Square.at(r-2, c-1)
+        ];
+
+        const moves = [];
+
+
+        for(const move of possibleMoves) {
+            if(move.row < 0 || move.row > 7 || move.col < 0 || move.col > 7) {
+                continue;
+            } else {
+                moves.push(move)
+            }
+        }
+
+
+        return moves;
     }
 }
